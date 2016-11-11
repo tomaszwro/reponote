419e3bbc8530545ac3293729a7d589b7c5bd6250
Tomasz Wróbel <tomekwr@gmail.com>
2016-11-11 06:31:36 +0100

    enable cors

--- packages/reponote-api/app/init/initExpressApp.js
+++ packages/reponote-api/app/init/initExpressApp.js
@@ -15,6 +15,12 @@ function initExpressApp( injectables ) {
   app.use( bodyParser.json() )
   app.use( bodyParser.urlencoded( { extended: false } ) )
 
+  app.use( ( req, res, next ) => {
+    res.header( "Access-Control-Allow-Origin", "*" )
+    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" )
+    next()
+  } )
+
   //
   // Routes
   //
